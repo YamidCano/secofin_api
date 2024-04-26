f7419179fff2e963064ea852e3eb188728acea03
AsistenteSecofin <asisitentesecofinsas@gmail.com>
2024-04-26 17:38:27 -0500

    Rutas para positios y payroll

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\PositionController;
+use App\HTTP\Controllers\PayrollController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -18,3 +20,21 @@ Route::prefix('employee')->group(function () {
     Route::put('/update/{id}', [EmployeeController::class, 'update']);
     Route::delete('/delete/{id}', [EmployeeController::class, 'destroy']);
 });
+
+Route::prefix('position')->group(function () {
+    Route::get('/', [PositionController::class, 'index']);
+    Route::post('/create', [PositionController::class, 'create']);
+    Route::get('/show/{id}', [PositionController::class, 'show']);
+    Route::put('/update/{id}', [PositionController::class, 'update']);
+    Route::delete('/delete/{id}', [PositionController::class, 'destroy']);
+});
+
+Route::prefix('payroll')->group(function () {
+    Route::get('/', [PayrollController::class, 'index']);
+    Route::post('/create', [PayrollController::class, 'create']);
+    Route::get('/show/{id}', [PayrollController::class, 'show']);
+    Route::put('/update/{id}', [PayrollController::class, 'update']);
+    Route::delete('/delete/{id}', [PayrollController::class, 'destroy']);
+});
+
+
